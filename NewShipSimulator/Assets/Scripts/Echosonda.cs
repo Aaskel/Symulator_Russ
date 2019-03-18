e022e6740e489f8662d0b223494da8cca84bd525
Symulator_Russ <48219955+Aaskel@users.noreply.github.com>
2019-03-18 09:03:39 +0100

    Update echosonda

--- NewShipSimulator/Assets/Scripts/Echosonda.cs
+++ NewShipSimulator/Assets/Scripts/Echosonda.cs
@@ -10,11 +10,20 @@ public class Echosonda : MonoBehaviour
     float opoznienie =5;
     void Update()
     {
+        System.Random rand = new System.Random();
+        double u1 = 1.0 - rand.NextDouble();
+        double u2 = 1.0 - rand.NextDouble();
+        double blad = ((System.Math.Sqrt(-2.0 * System.Math.Log(u1)) * System.Math.Sin(2.0 * System.Math.PI * u2)) * 0.3);
+        float bladconvert = (float)blad;
+        
         opoznienie += Time.deltaTime;
+        Debug.Log(blad);
+        Debug.Log(bladconvert);
         if (opoznienie >= 5)
         {
             if (Physics.Raycast(transform.position, Vector3.down, out hit))
-                depth = hit.distance;
+                depth = hit.distance +bladconvert ;
+            
             else
                 depth = -1;
 
